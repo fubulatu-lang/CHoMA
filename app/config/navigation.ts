dbf33f4539a83292b7be3725df1a7d01c5c98f75
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:02:35 +0000

    Create app/config/navigation.ts

---
+++ app/config/navigation.ts
@@ -0,0 +1,75 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+
+import PeopleIcon from "@mui/icons-material/People";
+
+import BadgeIcon from "@mui/icons-material/Badge";
+
+import ScienceIcon from "@mui/icons-material/Science";
+
+import MedicationIcon from "@mui/icons-material/Medication";
+
+import ReceiptIcon from "@mui/icons-material/Receipt";
+
+export const Navigation=[
+
+{
+
+title:"Dashboard",
+
+icon:DashboardIcon,
+
+path:"/dashboard"
+
+},
+
+{
+
+title:"Patients",
+
+icon:PeopleIcon,
+
+path:"/patients"
+
+},
+
+{
+
+title:"Employees",
+
+icon:BadgeIcon,
+
+path:"/employees"
+
+},
+
+{
+
+title:"Laboratory",
+
+icon:ScienceIcon,
+
+path:"/laboratory"
+
+},
+
+{
+
+title:"Pharmacy",
+
+icon:MedicationIcon,
+
+path:"/pharmacy"
+
+},
+
+{
+
+title:"Billing",
+
+icon:ReceiptIcon,
+
+path:"/billing"
+
+}
+
+];
