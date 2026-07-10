81615b7becc28b485b8f2c8b81acb52ad8814061
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:12:54 +0000

    Create src/components/layout/Shell/ThemeSwitcher.tsx

---
+++ src/components/layout/Shell/ThemeSwitcher.tsx
@@ -0,0 +1,11 @@
+import { IconButton } from "@mui/material";
+
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+
+export default function ThemeSwitcher() {
+  return (
+    <IconButton>
+      <DarkModeIcon />
+    </IconButton>
+  );
+}
