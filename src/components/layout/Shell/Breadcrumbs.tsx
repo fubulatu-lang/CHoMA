3dba564b925479b8b427b4f1de9ab8cdf613450c
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:11:39 +0000

    Create src/components/layout/Shell/Breadcrumbs.tsx

---
+++ src/components/layout/Shell/Breadcrumbs.tsx
@@ -0,0 +1,12 @@
+import {
+  Breadcrumbs,
+  Typography
+} from "@mui/material";
+
+export default function AppBreadcrumbs() {
+  return (
+    <Breadcrumbs>
+      <Typography>Home</Typography>
+    </Breadcrumbs>
+  );
+}
