21a9757b823029ace7e5d6cfeccfd923d958f4e2
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:01:17 +0000

    Create app/config/constants.ts

---
+++ app/config/constants.ts
@@ -0,0 +1,7 @@
+export const DATE_FORMAT = "dd/MM/yyyy";
+
+export const DATETIME_FORMAT = "dd/MM/yyyy HH:mm";
+
+export const DEFAULT_PAGE_SIZE = 25;
+
+export const MAX_UPLOAD_SIZE = 20 * 1024 * 1024;
