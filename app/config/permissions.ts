9064716b010ebec7a39146eb9ded5366fd5ecd3e
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 01:02:15 +0000

    Create app/config/permissions.ts

---
+++ app/config/permissions.ts
@@ -0,0 +1,23 @@
+export const Permissions={
+
+PATIENT_READ:"patient.read",
+
+PATIENT_CREATE:"patient.create",
+
+PATIENT_EDIT:"patient.edit",
+
+PATIENT_DELETE:"patient.delete",
+
+STAFF_READ:"staff.read",
+
+STAFF_CREATE:"staff.create",
+
+LAB_READ:"lab.read",
+
+LAB_CREATE:"lab.create",
+
+REPORT_READ:"report.read",
+
+SETTINGS:"settings"
+
+};
