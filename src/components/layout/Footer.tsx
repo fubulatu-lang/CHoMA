cd26a7656bf04dadfd788eab7ade8b39272dbb19
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 00:26:06 +0000

    Create src/components/layout/Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Footer(){
+
+ return(
+
+<Box
+sx={{
+textAlign:"center",
+py:2
+}}
+>
+
+<Typography variant="body2">
+
+© 2026 HRAPIMS
+
+</Typography>
+
+</Box>
+
+);
+
+}
