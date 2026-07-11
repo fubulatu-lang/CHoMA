8e95adf4a878aa4d0eb97cb95542d953681876d9
fubulatu-lang <fubulatu@gmail.com>
2026-07-11 15:00:03 +0000

    Create index.ts

---
+++ src/features/index.ts
@@ -0,0 +1,13 @@
+export * from "./patients";
+
+export * from "./employees";
+
+export * from "./attendance";
+
+export * from "./payroll";
+
+export * from "./reports";
+
+export * from "./settings";
+
+export * from "./auth";
