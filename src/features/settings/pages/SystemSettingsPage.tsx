0aea39706fbb6b8ecffd62774aa543c643783809
fubulatu-lang <fubulatu@gmail.com>
2026-07-11 09:37:23 +0000

    Create SystemSettingsPage.tsx

---
+++ src/features/settings/pages/SystemSettingsPage.tsx
@@ -0,0 +1,29 @@
+import { Stack } from "@mui/material";
+
+import PageContainer from "../../../components/common/PageContainer";
+import PageHeader from "../../../components/common/PageHeader";
+
+import SettingsForm from "../components/SettingsForm";
+
+export default function SystemSettingsPage() {
+
+    return (
+
+        <PageContainer>
+
+            <Stack spacing={3}>
+
+                <PageHeader
+                    title="System Settings"
+                    subtitle="Hospital system configuration"
+                />
+
+                <SettingsForm />
+
+            </Stack>
+
+        </PageContainer>
+
+    );
+
+}
