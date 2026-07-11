ca6a8c521511ed9398e83abc456707a1c29da898
fubulatu-lang <fubulatu@gmail.com>
2026-07-11 08:58:12 +0000

    Create index.ts to export settings modules

---
+++ src/features/settings/index.ts
@@ -0,0 +1,9 @@
+export * from "./api/settings.types";
+
+export * from "./api/settings.api";
+
+export * from "./hooks/useSettings";
+
+export * from "./store/settings.store";
+
+export * from "./validation/settings.schema";
